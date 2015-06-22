b4623ac3a288b988814360b781086d0c2d98d03f
Wenjie Chen <wjchen84@gmail.com>
2015-06-22 19:39:26 +0900

    rename moveit_config to h2r_moveit_config, and add kinect2_calibration launch file

--- baxter_kinect_calibration/scripts/setup_robot_left.sh
+++ baxter_kinect_calibration/scripts/setup_robot_left.sh
@@ -1,3 +1,4 @@
-rosrun baxter_tools camera_control.py -c left_hand_camera
+rosrun baxter_tools camera_control.py -c head_camera
+# rosrun baxter_tools camera_control.py -c left_hand_camera
 rosrun baxter_tools camera_control.py -o left_hand_camera -r 1280x800
 rosrun baxter_tools enable_robot.py -e
